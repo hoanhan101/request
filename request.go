d159224e95d4eb18ad2ba077b8eac8113a22209e
Hoanh An <hoanhan@bennington.edu>
2019-12-23 01:54:13 -0500

    add example to godoc

--- request.go
+++ request.go
@@ -7,6 +7,13 @@ import (
 	"net/url"
 )
 
+// Get issues a GET request to a given URL address and formats the response in
+// string. For example:
+// No query parameters
+//  r, err := request.Get("https://httpbin.org/get", nil)
+// With query parameters
+//  payload := map[string][]string{"k1": []string{"v1"}, "k2": []string{"v2", "v3"}}
+//  r, err := request.Get("https://httpbin.org/get", payload)
 func Get(address string, params map[string][]string) (string, error) {
 	res, err := get(address, params)
 	if err != nil {
@@ -22,6 +29,8 @@ func Get(address string, params map[string][]string) (string, error) {
 	return string(raw), nil
 }
 
+// GetJSON issues a GET request to a given URL address and formats the response
+// in JSON.
 func GetJSON(address string, params map[string][]string, scheme interface{}) error {
 	res, err := get(address, params)
 	if err != nil {
@@ -38,6 +47,8 @@ func GetJSON(address string, params map[string][]string, scheme interface{}) err
 	return nil
 }
 
+// Post issues a POST request to a given URL address and formats the response
+// in string.
 func Post(address string, body map[string][]string) (string, error) {
 	res, err := post(address, body)
 	if err != nil {
@@ -53,6 +64,8 @@ func Post(address string, body map[string][]string) (string, error) {
 	return string(raw), nil
 }
 
+// PostJSON issues a POST request to a given URL address and formats the response
+// in JSON.
 func PostJSON(address string, body map[string][]string, scheme interface{}) error {
 	res, err := post(address, body)
 	if err != nil {
